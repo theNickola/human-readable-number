d57ca0fcb4931bbab6d2412e27f6aabb2afee0a1
thenickola <kurilchik.nv@gmail.com>
2023-01-10 09:38:34 +0300

    feat: completed task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,12 @@
 module.exports = function toReadable (number) {
-  
+    let strNumber = String(number);
+    let mas19 = ["zero","one","two","three","four","five","six","seven","eight","nine","ten","eleven","twelve","thirteen","fourteen","fifteen","sixteen","seventeen","eighteen","nineteen"];
+    let masX = ["","ten","twenty","thirty","forty","fifty","sixty","seventy","eighty","ninety"];
+    return number <= 19 ? mas19[number] : 
+      strNumber.length === 2 && strNumber[1] === '0' ? masX[Number(strNumber[0])] : 
+        strNumber.length === 2 && strNumber[1] != '0' ? masX[Number(strNumber[0])] + " "+mas19[strNumber[1]]:
+          strNumber.length === 3 && strNumber[1] === '0' && strNumber[2] === '0' ? mas19[strNumber[0]] + " hundred" : 
+            strNumber.length === 3 && number%100 <= 19 ? mas19[strNumber[0]]+" hundred " + mas19[number%100]:
+              strNumber.length === 3 && strNumber[2] === '0' ? mas19[strNumber[0]]+" hundred " + masX[Number(strNumber[1])] :
+              mas19[strNumber[0]]+" hundred " + masX[Number(strNumber[1])] + " " + mas19[Number(strNumber[2])];
 }
